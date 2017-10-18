e55ac637e5e975def86d9e1e5826facc4bd433b1
smithwebtek <brad@smithwebtek.com>
2017-10-18 17:44:11 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+let repos, repoList
+
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function getRepos(user) {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://api.github.com/users/${user}/repos`)
+  debugger
+  req.send()
+}
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" data-owner="' + r.owner.login + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  console.log("Repo name: " + el.dataset.repo)
+  console.log("Repo owner: " + el.dataset.owner)
+  
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+
+
+  document.getElementById("commits").innerHTML = commitsList
+}
